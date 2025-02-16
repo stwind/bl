52410904484e878c9793d14e18cb514617e8a204
stwind <stwindfy@gmail.com>
2025-02-16 11:19:38 +0800

    add `node_tree.add_node`

--- src/bl/node_tree.py
+++ src/bl/node_tree.py
@@ -1,3 +1,12 @@
 def connect(tree, links):
     for k, v in links:
         tree.links.new(k, v)
+
+
+def add_node(tree, type, inputs={}, attrs={}):
+    node = tree.nodes.new(type)
+    for k, v in inputs.items():
+        node.inputs[k].default_value = v
+    for k, v in attrs.items():
+        setattr(node, k, v)
+    return node
