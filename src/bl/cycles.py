a0b681612dd108948a1c957c70dc3b65ad7a9d99
stwind <stwindfy@gmail.com>
2025-02-16 10:07:48 +0800

    update `bl.cycles`

--- src/bl/cycles.py
+++ src/bl/cycles.py
@@ -1,5 +1,6 @@
 def setup(prefs, compute_device_type="OPTIX"):
     cprefs = prefs.addons["cycles"].preferences
     cprefs.compute_device_type = compute_device_type
+    cprefs.get_devices()
     for d in cprefs.devices:
         d.use = d.type == compute_device_type
