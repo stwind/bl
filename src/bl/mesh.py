17ea4d7cc3ac8ba47730ea7269bc1a51b6278293
stwind <stwindfy@gmail.com>
2025-03-01 18:25:48 +0800

    update

--- src/bl/mesh.py
+++ src/bl/mesh.py
@@ -2,27 +2,43 @@ import numpy as np
 import bmesh
 
 
-def update_mesh(mesh):
+def update(mesh):
     bm = bmesh.new()
     bm.from_mesh(mesh)
     bm.to_mesh(mesh)
     bm.free()
 
 
-def get_verts(mesh):
+def get_vertices(mesh):
     verts = np.zeros((len(mesh.vertices) * 3), dtype=np.float32)
     mesh.vertices.foreach_get("co", verts)
     return verts.reshape(-1, 3)
 
 
+def get_triangles(mesh, dtype=np.uint32):
+    triangles = np.zeros((len(mesh.loop_triangles) * 3), dtype=dtype)
+    mesh.loop_triangles.foreach_get("vertices", triangles)
+    return triangles.reshape(-1, 3)
+
+
 def normalize_verts(mesh):
-    verts = get_verts(mesh)
+    verts = get_vertices(mesh)
     verts -= verts.mean(0)
     verts /= np.linalg.norm(verts, axis=-1).max()
     mesh.vertices.foreach_set("co", verts.ravel())
-    update_mesh(mesh)
+    update(mesh)
 
 
-def get_extent(mesh):
-    verts = get_verts(mesh)
-    return verts.min(0), verts.max(0)
+def get_aabb(mesh):
+    aabb = np.zeros((2, 3), dtype="f4")
+    for v in mesh.vertices:
+        aabb[0, 0], aabb[1, 0] = min(aabb[0, 0], v.co.x), max(aabb[1, 0], v.co.x)
+        aabb[0, 1], aabb[1, 1] = min(aabb[0, 1], v.co.y), max(aabb[1, 1], v.co.y)
+        aabb[0, 2], aabb[1, 2] = min(aabb[0, 2], v.co.z), max(aabb[1, 2], v.co.z)
+    return aabb
+
+
+def add(D, name, verts, faces):
+    mesh = D.meshes.new(name)
+    mesh.from_pydata(verts, [], faces)
+    return mesh
--- src/bl/object.py
+++ src/bl/object.py
@@ -23,3 +23,10 @@ def add_node_group_modifier(obj, name, group):
     mod = obj.modifiers.new(name=name, type="NODES")
     mod.node_group = group
     return mod
+
+
+def add_collection_instance(D, coll, name=None):
+    instance = D.objects.new(name=name or coll.name, object_data=None)
+    instance.instance_type = "COLLECTION"
+    instance.instance_collection = coll
+    return instance
