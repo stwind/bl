3cdba489777d3cff90dff43150073b9daa99ff4c
stwind <stwindfy@gmail.com>
2026-08-26 21:14:39 +0800

    update cycles

--- src/bl/cycles.py
+++ src/bl/cycles.py
@@ -1,4 +1,4 @@
-def setup(prefs, compute_device_type="CUDA"):
+def setup(prefs, compute_device_type="OPTIX"):
     cprefs = prefs.addons["cycles"].preferences
     cprefs.compute_device_type = compute_device_type
     cprefs.get_devices()
